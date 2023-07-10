ee9ad3fb7f414c769acca52dbb11a53b97c3a9fa
alex <alexfasmod@mail.ru>
2023-07-10 19:18:12 +0500

    Add enum and struct in menu.h

--- src/menu.h
+++ src/menu.h
@@ -2,6 +2,27 @@
 #define __APP_MAIN_H
 #include "main.h"
 
+typedef enum {
+    MAIN_MENU,
+    ACCELX,
+    ACCELY,
+    ACCELZ,
+
+    GYROX,
+    GYROY,
+    GYROZ,
+
+    TEMP,
+
+    ALL_MENU,
+}MENUS;
+
+typedef struct {
+    uint8_t col;
+    uint8_t row;
+    MENUS prev_menu;
+    MENUS cur_menu;
+}menu_t;
 
 
 
